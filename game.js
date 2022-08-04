1e0a6e316fedeb9f079fbae6124f14c00a956342
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-08-04 17:54:11 -0600

    Fixed functions to display winner in consoleLog

--- game.js
+++ game.js
@@ -3,16 +3,16 @@ class Game {
     this.player1 = player1
     this.player2 = player2
     this.turn = 1
-    this.player1Turn= false
+    this.player1Turn = false
     this.winner = undefined
     this.board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   }
 
-    // board
-    // which field the player is choosing
-    // X whose turn
-    // function win conditions
-    // function check the current player
+  // board
+  // which field the player is choosing
+  // X whose turn
+  // function win conditions
+  // function check the current player
 
   checkMove(event) {
     this.turn++
@@ -24,40 +24,46 @@ class Game {
       //this.player2
       console.log('ptwoooooo')
     }
-    if (this.turn >= 4){
-      this.checkWin()
-    }
   }
-  checkWin(moves){
+
+  checkWin() {
     console.log('checking win')
     var winnerStates = [
-      [1,2,3],
-      [4,5,6],
-      [7,8,9],
-      [1,4,7],
-      [2,5,8],
-      [3,6,9],
-      [1,5,9],
-      [3,5,7]
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+      [1, 5, 9],
+      [3, 5, 7]
     ]
     for (var i = 0; i < winnerStates.length; i++) {
-    if ((player2.moves.includes(winnerStates[i][0])) &&
-    (player2.moves.includes(winnerStates[i][1])) &&
-    (player2.moves.includes(winnerStates[i][2]))) {
-      console.log('player 1 wins')
-    }
+      console.log('heyyyyyyyyyyyyyyyyyy')
+      if ((player1.moves.includes(winnerStates[i][0])) &&
+        (player1.moves.includes(winnerStates[i][1])) &&
+        (player1.moves.includes(winnerStates[i][2]))) {
+        player1.wins++
+        console.log('player 1 wins')
+      }
+      if ((player2.moves.includes(winnerStates[i][0])) &&
+        (player2.moves.includes(winnerStates[i][1])) &&
+        (player2.moves.includes(winnerStates[i][2]))) {
+        player2.wins++
+        console.log('player 2 wins')
+      }
     }
   }
   //check the array of wins to see if boards matches
   //update DOM to show winner
   //how do i tell if a player has moved and how to represent the move
 
-resetGame(){
-  this.turn = 1
-  this.player1turn = false
-}
-
-
+  resetGame() {
+    this.turn = 1
+    this.player1turn = false
+    this.player1.moves = []
+    this.player2.moves = []
+  }
 }
 //update dom of players move
 
--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 // Query selectors
 var gameBoard = document.querySelector('.game-box')
 var playerTurn = document.querySelector('.player-turn')
@@ -18,20 +17,28 @@ var game = new Game(player1, player2)
 //   addPiece()
 // }
 
-function addPiece(event){
-  console.log(event.target.dataset.section)//checking the value of the section
+function addPiece(event) {
   game.checkMove()
+  console.log(event.target.dataset.section) //checking the value of the section
   console.log(game.player1Turn)
-  if (game.player1Turn === true){
-  event.target.innerText = 'X'
-  player1.moves.push(parseInt(event.target.dataset.section))
-  console.log(player1.moves)
-} else {
-  event.target.innerText = 'O'
-  player2.moves.push(parseInt(event.target.dataset.section))
-  console.log(player2.moves)
-}
+  if (game.player1Turn === true) {
+    event.target.innerText = 'X'
+    player1.moves.push(parseInt(event.target.dataset.section))
+    console.log(player1.moves)
+    game.checkWin()
+  } else {
+    event.target.innerText = 'O'
+    player2.moves.push(parseInt(event.target.dataset.section))
+    console.log(player2.moves)
+    game.checkWin()
+  }
 }
 
+function playerHasWon() {
+
+}
+//displays playerTurn
+//displays winnerStates
+//resets game
   //replace what is there with an x o
   //what are we clicking on
