5252f92558c54c8912b516515ec14043344d4ac0
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-08-05 16:46:57 -0600

    Added functions to reset game and gameboard

--- game.js
+++ game.js
@@ -21,13 +21,12 @@ class Game {
       console.log('p1')
     } else {
       this.player1Turn = false
-      //this.player2
       console.log('ptwoooooo')
     }
   }
 
   checkWin() {
-    console.log('checking win')
+    //console.log('checking win')
     var winnerStates = [
       [1, 2, 3],
       [4, 5, 6],
@@ -39,39 +38,41 @@ class Game {
       [3, 5, 7]
     ]
     for (var i = 0; i < winnerStates.length; i++) {
-      console.log('heyyyyyyyyyyyyyyyyyy')
+      //console.log('heyyyyyyyyyyyyyyyyyy')
       if ((player1.moves.includes(winnerStates[i][0])) &&
         (player1.moves.includes(winnerStates[i][1])) &&
         (player1.moves.includes(winnerStates[i][2]))) {
         player1.wins++
-        console.log('player 1 wins')
+        this.winner = true
+        console.log(`PLAYER 1 WINS!!!`)
       }
       if ((player2.moves.includes(winnerStates[i][0])) &&
         (player2.moves.includes(winnerStates[i][1])) &&
         (player2.moves.includes(winnerStates[i][2]))) {
         player2.wins++
-        console.log('player 2 wins')
+        this.winner = false
+        console.log(`PLAYER 2 WINS!!!`)
       }
     }
   }
   //check the array of wins to see if boards matches
   //update DOM to show winner
   //how do i tell if a player has moved and how to represent the move
-
-  resetGame() {
+  resetGameBoard() {
     this.turn = 1
     this.player1turn = false
     this.player1.moves = []
     this.player2.moves = []
+    this.winner = undefined
+  }
+  checkDraw(){
+    if(this.turn = 10){
+      return true
+    }
   }
 }
+
 //update dom of players move
-
-
-//  }
-  //checkDraw(){
-
-//  }
-  // if a player chooses a square another player choses a square once if
-  // a player has made 3 or more choices
-  //check to see if they one if not allow thme to make another move
+// if a player chooses a square another player choses a square once if
+// a player has made 3 or more choices
+//check to see if they one if not allow thme to make another move
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 // Query selectors
 var gameBoard = document.querySelector('.game-box')
 var playerTurn = document.querySelector('.player-turn')
-
+var gameSpaces = document.querySelectorAll('td')
+var title = document.querySelector('h1')
 
 
 //Event listener
@@ -13,9 +14,6 @@ var player2 = new Player()
 var game = new Game(player1, player2)
 
 //Functions
-// function loadPage(){
-//   addPiece()
-// }
 
 function addPiece(event) {
   game.checkMove()
@@ -24,21 +22,40 @@ function addPiece(event) {
   if (game.player1Turn === true) {
     event.target.innerText = 'X'
     player1.moves.push(parseInt(event.target.dataset.section))
+    title.innerText= `It's Player 1's move`
     console.log(player1.moves)
     game.checkWin()
   } else {
     event.target.innerText = 'O'
     player2.moves.push(parseInt(event.target.dataset.section))
+    title.innerText= `It's Player 1's move`
     console.log(player2.moves)
     game.checkWin()
   }
+    playerHasWon()
 }
 
 function playerHasWon() {
-
+  if (game.winner === true) {
+    title.innerText= 'Player 1 Wins!!!'
+  }
+  if (game.winner === false) {
+    title.innerText= 'Player 2 Wins!!!'
+    resetGameSpaces() //reset board
+  }
 }
+
+function resetGameSpaces() {
+  for (var i = 0; i < gameSpaces.length; i++) {
+    gameSpaces[i].innerText = ''
+  }
+   game.resetGameBoard() //reset board
+}
+
+
 //displays playerTurn
 //displays winnerStates
 //resets game
-  //replace what is there with an x o
-  //what are we clicking on
+//replace what is there with an x o
+//what are we clicking on
+// need to work on bugs can click again on square
