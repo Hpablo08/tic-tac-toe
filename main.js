0fe624b10cd84552cb925cd2e64ba5bb913bb1ac
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-08-07 15:44:28 -0600

    Added a change player feature and fixed active bugs

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var playerOneScore = document.querySelector('.player-one-score')
 var playerTwoScore = document.querySelector('.player-two-score')
 var notValidSpace = document.querySelector('tbody')
 
+
 //Event listener
 gameBoard.addEventListener('click', addPiece)
 window.addEventListener('load', scoreKeeper)
@@ -17,14 +18,7 @@ var player2 = new Player('player2', 'O')
 var game = new Game(player1, player2)
 
 //Functions
-// function squareAvailability(targetedSpot){
-//   if(targetedSpot === notValidSpace){
-//     alert('Please click a valid spot')
-//     //return true
-//   } else {
-//     addPiece()
-//   }
-//   }
+
 function scoreKeeper(){
   player1.retrieveWinsFromStorage()
   player2.retrieveWinsFromStorage()
@@ -33,41 +27,52 @@ function scoreKeeper(){
 }
 
 function addPiece(event) {
-    game.checkMove()
-    console.log(event.target.dataset.section) //checking the value of the section
-    console.log(game.player1Turn)
+  if (event.target.textContent.includes('X') || event.target.textContent.includes('O') || event.target.innerText === notValidSpace) {
+    return alert('Please click a valid spot')
+  }
+  // console.log(event.target.dataset.section) //checking the value of the section
+  // console.log(game.player1Turn)
   if (game.player1Turn === true) {
     event.target.innerText = 'X'
     player1.moves.push(parseInt(event.target.dataset.section))
-    title.innerText= `It's Player 2's move`
+    title.innerText = `It's Player 2's move`
     console.log(player1.moves)
+    game.checkMove()
     game.checkWin()
   } else {
     event.target.innerText = 'O'
     player2.moves.push(parseInt(event.target.dataset.section))
-    title.innerText= `It's Player 1's move`
+    title.innerText = `It's Player 1's move`
     console.log(player2.moves)
+    game.checkMove()
     game.checkWin()
   }
-    playerHasWon()
+  playerHasWon()
 }
-
-
+//
+// function squareAvailability(event){
+//   if(event.target.textContent.includes('X') || event.target.textContent.includes('O')) {
+//      alert('Please click a valid spot')
+//   }
+// }
 function playerHasWon() {
-    game.checkDraw()
+  game.checkDraw()
   if (game.winner === 'player1') {
     title.innerText = 'Player 1 Wins!!!'
     playerOneScore.innerText = `${player1.wins}`
-    resetGameSpaces()
+    gameBoard.classList.add('hidden')
+    setTimeout(resetGameSpaces, 3000)
   }
   if (game.winner === 'player2') {
     title.innerText= 'Player 2 Wins!!!'
     playerTwoScore.innerText = `${player2.wins}`
-    resetGameSpaces() //reset board
+    gameBoard.classList.add('hidden')
+    setTimeout(resetGameSpaces, 3000) //reset board
   }
   if (game.winner === 'draw'){
     title.innerText= "It's a Draw! play again?"
-    resetGameSpaces()
+    gameBoard.classList.add('hidden')
+    setTimeout(resetGameSpaces, 3000)
   }
 }
 
@@ -75,14 +80,6 @@ function resetGameSpaces() {
   for (var i = 0; i < gameSpaces.length; i++) {
     gameSpaces[i].innerText = ''
   }
-    game.resetGameBoard() //reset board
+    gameBoard.classList.remove('hidden')
+    game.resetGameBoard()
   }
-
-
-
-//displays playerTurn
-//displays winnerStates
-//resets game
-//replace what is there with an x o
-//what are we clicking on
-// need to work on bugs can click again on square
--- player.js
+++ player.js
@@ -4,15 +4,9 @@ class Player{
     this.marker = marker
     this.wins = 0
     this.moves= []
-    // is turn set to a boolean
-    // what symbol am I?
-    // make a move
-    // how many times I've won?
-    // add local storage
   }
   saveWinsToStorage(){
     localStorage.setItem(`${this.id}`, JSON.stringify(this.wins))
-
   }
   retrieveWinsFromStorage(){
     var storedWins = JSON.parse(localStorage.getItem(`${this.id}`)) || []
